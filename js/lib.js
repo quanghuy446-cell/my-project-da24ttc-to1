4b87da563e6ab4e518d5ce4366d6175d3adce0d9
Nguyễn Quang Huy <quanghuytv29@gmail.com>
2026-05-19 00:19:02 +0700

    upd

--- js/lib.js
+++ js/lib.js
@@ -7,41 +7,40 @@
     {id:"6", name:"Dao gọt", price: 350000, image:"../assets/images/dao-got.jpg", productLink:"product-detail.html"},
     {id:"7", name:"Dao chặt", price: 400000, image:"../assets/images/dao-chat.jpg", productLink:"product-detail.html"},
 ]
+function loadProduct(products)
+{
+    for(let i = 0; i < products.length; i++)
+    {
+        addProduct(products[i].id, products[i].name, products[i].price, products[i].image, products[i].productLink);
+    }
+}
 function addProduct(id, name, price, image, hyperLink)
 {
-    //tạo khung chứa 1 item
     const productitem = document.createElement("div");
     productitem.setAttribute("class", "product-item");
-    //tạo khung chứa hình
     const productImage = document.createElement("div");
     productImage.setAttribute("class", "product-image");
-    //tạo đối tượng hình ảnh
     const Image = document.createElement("img");
     Image.setAttribute("src", image);
     Image.setAttribute("alt", name);
-    //gắn hình vào khung
     productImage.appendChild(Image);
     const productInfo = document.createElement("div");
     productInfo.setAttribute("class", "product-info");
-    //tạo paragraph 1
     const productName = document.createElement("p");
-    productNameText = document.createTextNode(name);
+    const productNameText = document.createTextNode(name);
     productName.appendChild(productNameText);
-    //tạo paragraph 2
     const productPrice = document.createElement("p");
-    productPriceText = document.createTextNode(price);
+    const productPriceText = document.createTextNode(price + "/con");
     productPrice.appendChild(productPriceText);
-    //hyperLink
     const productLink = document.createElement("a");
     const productLinkText = document.createTextNode("Xem chi tiết");
     productLink.appendChild(productLinkText);
-    productLink.setAttribute("href", hyperLink);
-    //thêm vào khung chứa thông tin
+    productLink.setAttribute("href", hyperLink + "?id=" + id);
     productInfo.appendChild(productName);
     productInfo.appendChild(productPrice);
     productInfo.appendChild(productLink);
-    //thêm vào khung chứa 1 item
     productitem.appendChild(productImage);
     productitem.appendChild(productInfo);
     document.getElementById("product-list").appendChild(productitem);
 }  
+loadProduct(productlist);
